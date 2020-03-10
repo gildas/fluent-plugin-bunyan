d7b0dc630d0d8d7d72c184484efd99828600ca88
Gildas Cherruel <gildas@breizh.org>
2020-03-11 02:25:21 +0900

    add more test about complex properties

--- test/plugin/test_parser_bunyan.rb
+++ test/plugin/test_parser_bunyan.rb
@@ -53,6 +53,32 @@ class BunyanParserTest < Test::Unit::TestCase
         assert_equal(expected_time,   time)
       end
     end
+
+    test "can parse entry with complex custom properties" do
+      driver = create_driver(CONFIG)
+      text = '{"hostname":"helloworld-go-helloworld-5649dfd7bd-k8954","level":50,"msg":"Failed to be happy","name":"helloworld","pid":1,"err":{"text": "aouch", "code": 404},"reqid":"0ba850ff-1e1f-4bcf-8288-135927bb850f","scope":"start","tid":21,"time":"2020-03-10T15:04:19Z","topic":"server","v":0}'
+      expected_time   = Time.utc(2020, 3, 10, 15, 4, 19).to_i
+      expected_record = {
+        "host"     => "helloworld-go-helloworld-5649dfd7bd-k8954",
+        "name"     => "helloworld",
+        "severity" => "error",
+        "level"    => 50,
+        "message"  => "Failed to be happy",
+        "topic"    => "server",
+        "scope"    => "start",
+        "pid"      => 1,
+        "tid"      => 21,
+        "reqid"    => "0ba850ff-1e1f-4bcf-8288-135927bb850f",
+        "err"      => {
+          "code" => 404,
+          "text" => "aouch",
+        },
+      }
+      driver.instance.parse(text) do |time, record|
+        assert_equal(expected_record, record)
+        assert_equal(expected_time,   time)
+      end
+    end
   end
 
   private
